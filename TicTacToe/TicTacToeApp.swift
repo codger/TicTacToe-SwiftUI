9973d7ae236f4064dd5534e0ef972c678fcd08b6
John James <jjames@pacbell.net>
2020-08-05 17:43:43 -0700

    Initial Commit

---
+++ TicTacToe/TicTacToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeApp.swift
+//  TicTacToe
+//
+//  Created by John James Retina on 8/5/20.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
