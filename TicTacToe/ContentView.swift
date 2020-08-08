54168fb3eb935ea5048e1b7d4ee18d2daeb1aac0
John James <jjames@pacbell.net>
2020-08-07 22:01:02 -0700

    Working

--- TicTacToe/ContentView.swift
+++ TicTacToe/ContentView.swift
@@ -7,14 +7,88 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!").padding()
-    }
-}
+var winRows = [[0,1,2],[3,4,5],[6,7,8], // Horizontal
+               [0,3,6],[1,4,7],[2,5,8], // Vertical
+               [0,4,8],[2,6,6]]         // Diagonal
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+
+struct ContentView: View {
+  @State var squares = [ 1,1,0,2,2,1,0,1,2]
+  @State var activePlayer = true
+  @State var winner = false
+  
+  var body: some View {
+    NavigationView {
+      ZStack{
+        Color(.darkGray)
+        VStack{
+          HStack{
+            Text("Player Turn :")
+            Square(shape: activePlayer == true ? 1 : 2)
+              .stroke(activePlayer ? Color.red: Color.green, style: StrokeStyle(lineWidth: 20, lineCap: .round, lineJoin: .round))
+              .background(Color.white)
+              .cornerRadius(15)
+              .aspectRatio(contentMode: .fit)
+          }.foregroundColor(.white)
+          .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: 100)
+          .font(.largeTitle)
+          ForEach((0...2), id: \.self) { col in
+            HStack{
+              ForEach((0...2), id: \.self) { row in
+                Square(shape: squares[row + 3 * col])
+                  .stroke(squares[row + 3 * col] == 1 ? Color.red: Color.green, style: StrokeStyle(lineWidth: 20, lineCap: .round, lineJoin: .round))
+                  .background(Color.white)
+                  .cornerRadius(15)
+                  .aspectRatio(contentMode: .fit)
+                  .onTapGesture(perform: {
+                      if squares[row + 3 * col] == 0 && !winner{
+                        squares[row + 3 * col] = activePlayer == true ? 1: 2
+                        haveWinner()
+                        activePlayer.toggle()
+                      }
+                  })
+              }
+            }
+          }
+          HStack{
+            Text( winner ? "Have a winner" : "No Winner Yet")
+              .font(.largeTitle)
+              .background(Color.white)
+          }
+        }.padding()
+      }
+      .navigationBarTitle("Tic-tac-toe", displayMode: .inline)
+      .navigationBarItems(trailing:
+                            Button("🔄")
+                            {
+                              squares = [ 0,0,0,0,0,0,0,0,0]
+                              winner = false
+                            }
+      )
     }
+  }
+  func squarePressed(num : Int) {
+    print(num)
+  }
+  func haveWinner(){
+    
+    for row in winRows {
+      print("\(squares[row[0]]) \(squares[row[1]]) \(squares[row[2]])")
+
+      if squares[row[0]] == squares[row[1]] {
+        if squares[row[0]] == squares[row[2]] {
+          if squares[row[0]] != 0 {
+            print("\(squares[row[0]]) \(squares[row[1]]) \(squares[row[2]])")
+            winner = true
+          }
+        }
+      }
+    }
+  }
 }
+  
+  struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+      ContentView()
+    }
+  }
---
+++ TicTacToe/Square.swift
@@ -0,0 +1,41 @@
+//
+//  Square.swift
+//  TicTacToe
+//
+//  Created by John James Retina on 8/5/20.
+//
+
+import SwiftUI
+
+struct Square: Shape {
+  var shape : Int
+  func path(in rect: CGRect) -> Path {
+    var path = Path()
+    let inset : CGFloat = 20.0
+    if shape == 1{
+      path.move(to: CGPoint(x: rect.minX + inset, y: rect.maxY - inset))
+      path.addLine(to: CGPoint(x: rect.maxX - inset, y: rect.minY + inset))
+      path.move(to: CGPoint(x: rect.minX + inset, y: rect.minY + inset))
+      path.addLine(to: CGPoint(x: rect.maxX - inset, y: rect.maxY - inset))
+    }
+    else if shape == 2 {
+      path.addArc(center: CGPoint(x: rect.midX, y: rect.midY), radius: rect.midX - (inset), startAngle: Angle(radians: 0), endAngle: Angle(radians: .pi * 2), clockwise: true)
+    }
+    return path
+  }
+}
+
+struct Square_Previews: PreviewProvider {
+  static var previews: some View {
+    GeometryReader {geom in
+      ZStack{
+      Square(shape: 1)
+        .stroke(Color.red, style: StrokeStyle(lineWidth: geom.size.width / 10.0, lineCap: .round, lineJoin: .round))
+        .aspectRatio(1, contentMode: .fit)
+        Square(shape: 2)
+          .stroke(Color.red, style: StrokeStyle(lineWidth: geom.size.width / 10.0, lineCap: .round, lineJoin: .round))
+          .aspectRatio(1, contentMode: .fit)
+    }
+    }
+  }
+}
